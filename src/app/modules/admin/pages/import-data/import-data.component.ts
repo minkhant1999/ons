2a270690f1fba7e2c2c5d3e2f13d1e0e5f5dfabf
Min Khant <minkhantmgmg@mytel.com.mm>
2024-10-29 16:18:52 +0630

    import data done

--- src/app/modules/admin/pages/import-data/import-data.component.ts
+++ src/app/modules/admin/pages/import-data/import-data.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { PaginatorComponent } from '../ui-management/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-import-data',
   templateUrl: './import-data.component.html',
-  styleUrls: ['./import-data.component.scss']
+  styleUrls: ['./import-data.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatIconModule, PaginatorComponent],
 })
-export class ImportDataComponent {
-
-}
+export class ImportDataComponent {}
