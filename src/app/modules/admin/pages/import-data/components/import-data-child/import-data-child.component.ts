28693646ec18373f108771cd5d7c14d1b529b514
unknown <sithumoemyint.1331@gmail.com>
2024-10-29 23:41:34 +0630

    implemented model

--- src/app/modules/admin/pages/import-data/components/import-data-child/import-data-child.component.ts
+++ src/app/modules/admin/pages/import-data/components/import-data-child/import-data-child.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { PaginatorComponent } from 'src/app/modules/custom/paginator/paginator.component';
+import { AlertService } from 'src/app/modules/service/alert.service';
 
 @Component({
   selector: 'app-import-data-child',
@@ -10,4 +11,46 @@ import { PaginatorComponent } from 'src/app/modules/custom/paginator/paginator.c
   standalone: true,
   imports: [CommonModule, MatIconModule, PaginatorComponent],
 })
-export class ImportDataChildComponent {}
+export class ImportDataChildComponent implements OnInit {
+  public tableData = [
+    {
+      id: 1,
+      fileName: 'file1.txt',
+      reason: 'Check details',
+      date: '26/10/2024',
+      status: 'Processing',
+    },
+    {
+      id: 2,
+      fileName: 'file2.txt',
+      reason: 'Check details',
+      date: '27/10/2024',
+      status: 'Fail',
+    },
+    {
+      id: 3,
+      fileName: 'file3.txt',
+      reason: 'Check details',
+      date: '28/10/2024',
+      status: 'Imported',
+    },
+  ];
+
+  constructor(private _alert: AlertService) {}
+
+  ngOnInit(): void {}
+
+  onDelete() {
+    this._alert
+      .deleteNotification(
+        'Are u sure?',
+        'Do you want to delete this file.',
+        'SURE'
+      )
+      .subscribe((result: boolean) => {
+        if (result) {
+          console.log(result, ' hello world');
+        }
+      });
+  }
+}
