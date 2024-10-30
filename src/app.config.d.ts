0a5b86c834fb3b7e94411e7eba556b19f6f6de2f
swanyeehtet <swanyeehtet1@mytel.com.mm>
2024-10-30 14:18:28 +0630

    login api done

--- src/app.config.d.ts
+++ src/app.config.d.ts
@@ -1,6 +1,7 @@
 declare module 'src/assets/app.config' {
   export const SERVICE_URLS: {
     LOGIN: string;
+    CONFIRM_LOGIN: string;
     STATISTIC: string;
     GET_ALL_IMPORT_FILE: string;
     IMPORT_FILE: string;
