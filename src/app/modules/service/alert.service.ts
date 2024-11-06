a65e801f724adf76193fd61986e830f378516313
eaintthet36 <eaintthetpainghmuekhin@gmail.com>
2024-11-06 14:29:28 +0630

    popup width fixed

--- src/app/modules/service/alert.service.ts
+++ src/app/modules/service/alert.service.ts
@@ -61,6 +61,13 @@ export class AlertService {
   }
 
   deleteNotification(title: string, des: string, type: string) {
+    const screenWidth = window.innerWidth;
+    let dialogWidth = '35%';
+    if (screenWidth <= 430) {
+      dialogWidth = '90%';
+    } else if (screenWidth <= 1024) {
+      dialogWidth = '50%';
+    }
     const _modal = this.dialog.open(DeleteComponent, {
       width: '35%',
       disableClose: false,
@@ -79,6 +86,8 @@ export class AlertService {
     let dialogWidth = '35%';
     if (screenWidth <= 430) {
       dialogWidth = '90%';
+    } else if (screenWidth <= 1024) {
+      dialogWidth = '50%';
     }
     const _modal = this.dialog.open(ConfirmSuccessFailComponent, {
       width: dialogWidth,
