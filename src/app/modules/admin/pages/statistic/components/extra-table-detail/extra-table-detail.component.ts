8a44a79b3fe8ba2f1b09f024766bb77a86f2b1bc
Eaint Thet <eaintthetpainghmuekhin@gmail.com>
2024-11-24 11:06:45 +0630

    fixed UI

--- src/app/modules/admin/pages/statistic/components/extra-table-detail/extra-table-detail.component.ts
+++ src/app/modules/admin/pages/statistic/components/extra-table-detail/extra-table-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { StatisticService } from '../../statistic.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-extra-table-detail',
@@ -13,15 +14,19 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ExtraTableDetailComponent implements OnInit {
   vmy: any;
+  public conditionRole = '';
   constructor(
     private service: StatisticService,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _dialogRef: MatDialogRef<ExtraTableDetailComponent>
+    private _dialogRef: MatDialogRef<ExtraTableDetailComponent>,
+    private cookieService: CookieService
   ) {
     this.vmy = data.vmy;
   }
   extraTableData: any[] = [];
   ngOnInit(): void {
+    this.conditionRole = this.cookieService.get('role');
+
     this.service
       .extraTable({ fbbLeaderVMY: this.vmy })
       .subscribe((data: any) => {
